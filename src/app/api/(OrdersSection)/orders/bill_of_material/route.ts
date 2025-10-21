7e3320bc142fc5b5b874b04ac09bda75dcc9008a
elhanbthomas <elhanbthomas@gmail.com>
2025-10-21 21:56:54 +0530

    additional details

--- src/app/api/(OrdersSection)/orders/bill_of_material/route.ts
+++ src/app/api/(OrdersSection)/orders/bill_of_material/route.ts
@@ -3,6 +3,10 @@ import { extractPenDetails } from "@/app/lib/configuratorFunctions";
 import { expandCustomer} from "@/app/lib/customerFunctions";
 import { getAmountDetails, checkInventory } from "@/app/lib/orderFunction";
 
+import stone_prices from "@/app/data/stone-price.json"
+import amount_details from "@/app/data/amount-details.json"
+import time_details from "@/app/data/time-details.json"
+
 export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
     const workOrderId = searchParams.get("work_order_id");
@@ -33,7 +37,10 @@ export async function GET(request: Request) {
         customer: await expandCustomer(data[0].customer_id),
         isPaid: data[0].isPaid,
         AllMaterials: data[0].material_wts,
-        requiredMaterials: requiredMaterialsAndWts
+        requiredMaterials: requiredMaterialsAndWts,
+        material_prices: stone_prices,
+        amount_details: amount_details,
+        time_details: time_details
     }
 
     return Response.json(finalData)
