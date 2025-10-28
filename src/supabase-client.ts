e707292250d6b4339d9493ad52c355b6f8cc9fc7
elhanbthomas <elhanbthomas@gmail.com>
2025-10-28 21:25:37 +0530

    authentication route handlers

--- src/supabase-client.ts
+++ src/supabase-client.ts
@@ -1,4 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { cookies } from "next/headers";
 
 const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL as string;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
@@ -6,3 +8,20 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 export const supabase = createClient(
     supabaseURL, supabaseAnonKey
 );
+
+export const serverClient = async () => {
+    const cookieStore = await cookies();
+    return createServerClient(supabaseURL, supabaseAnonKey, {
+        cookies: {
+            get(name: string) {
+                return cookieStore.get(name)?.value;
+            },
+            set(name: string, value: string, options?: CookieOptions) {
+                cookieStore.set({ name, value, ...options });
+            },
+            remove(name: string, options?: CookieOptions) {   
+                cookieStore.set({ name, value: '', ...options });
+            }
+        }
+    });
+}
